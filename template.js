45488e330b7e79455aa5fc7a76123dfedf9900df
Edward Bebbington <edward.bebbington@intericty.technology>
2019-06-02 22:29:21 +0100

    Corrected last empty variable. I promise.

--- template.js
+++ template.js
@@ -4,7 +4,7 @@ const searchFile = require('./search-file.js').searchContextFile
 const data = [
   'hello.world.'
 ]
-const fileToRead = 'test.txt'
-const fileToWrite = 'test2.txt'
+const fileToRead = 'template.txt'
+const fileToWrite = 'template-new.txt'
 // Call the function
 searchFile(data, fileToRead, fileToWrite)
