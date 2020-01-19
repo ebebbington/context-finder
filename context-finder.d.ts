f7b7cfc7218a540438eae87622044d7d37452ac9
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-19 19:20:52 +0000

    Added a .d.ts file for the main function

---
+++ context-finder.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} contextTitles An array of strings that hold the context titles to grab
+ * @param {string} fileToRead The file to read e.g. './myconfs.conf'
+ * @param {stirng} fileToWrite The file to write to e.g. './newConfs.conf'
+ */
+declare function readAndPrint (contextTitles: string[], fileToRead: string, fileToWrite: string): void
+
+export = readAndPrint
