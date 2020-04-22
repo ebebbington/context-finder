27538958d736ac1c69d175af3e7b733af556ed9e
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-22 02:52:31 +0100

    Moved index.js to TS

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const contextFinder = require('./context-finder.js')
-
-function runFromCommandLine () {
-	// Check if --help argument is used
-	if (process.argv[2] === '--help') {
-		console.log("\x1b[32mUsage: node index.js <file to read> <file to write to> <context> <context> ...\x1b[0m")
-	}
-	
-	// Check command line arguments are set (minimum is 5: node index.js <file> <file> <context>)
-	if (process.argv.length < 5) {
-		return false
-	}
-
-	// Initialise data to be passed into function call
-	const contextTitles = (process.argv).splice(4, (process.argv.length - 1)) // get array of values where context titles should be
-	const fileToRead = process.argv[2]
-	const fileToWrite = process.argv[3]
-
-	// Check fileToRead exists
-	const fileToReadExists = (require('fs')).existsSync(fileToRead)
-	if (!fileToReadExists) {
-		return false
-	}
-
-	// Call the function
-	contextFinder(contextTitles, fileToRead, fileToWrite)
+"use strict";
+var contextFinder = require('./context-finder.js');
+function runFromCommandLine() {
+    // Check if --help argument is used
+    if (process.argv[2] === '--help') {
+        console.log("\x1b[32mUsage: node index.js <file to read> <file to write to> <context> <context> ...\x1b[0m");
+    }
+    // Check command line arguments are set (minimum is 5: node index.js <file> <file> <context>)
+    if (process.argv.length < 5) {
+        return false;
+    }
+    // Initialise data to be passed into function call
+    var contextTitles = (process.argv).splice(4, (process.argv.length - 1)); // get array of values where context titles should be
+    var fileToRead = process.argv[2];
+    var fileToWrite = process.argv[3];
+    // Check fileToRead exists
+    var fileToReadExists = (require('fs')).existsSync(fileToRead);
+    if (!fileToReadExists) {
+        return false;
+    }
+    // Call the function
+    contextFinder(contextTitles, fileToRead, fileToWrite);
 }
-
 /* If ran from the command line */
 if (require.main === module) {
-    runFromCommandLine()
-    return false
+    runFromCommandLine();
 }
-
-// If required as an npm module
-module.exports = contextFinder
+else {
+    // If required as an npm module
+    module.exports = contextFinder;
+}
