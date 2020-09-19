a8c8dd2be833d68c7a08f0e235c0dc9d3534c474
Edward Bebbington <edward.bebbington@intercity.technology>
2020-09-19 01:51:11 +0100

    fix deno tests

--- tests/deno/cli/context_finder_test.ts
+++ tests/deno/cli/context_finder_test.ts
@@ -1,18 +1,18 @@
-import { assertEquals } from "../../deps.ts"
-import { fileExists } from "../../src/deno/helpers.ts";
+import { assertEquals } from "../../../deps.ts"
+import { fileExists } from "../../../src/deno/helpers.ts";
 
 Deno.test({
     name: 'Should do nothing if the file to read doesn\'t exist',
     async fn(): Promise<void> {
         const p = await Deno.run({
-            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../mod.ts', 'idontexist', 'write.txt', 'admin'],
-            cwd: "./tests"
+            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../../../mod.ts', 'idontexist', 'write.txt', 'admin'],
+            cwd: "./tests/deno/cli"
         })
         const status = await p.status()
         assertEquals(status.code, 1)
         assertEquals(status.success, false)
         p.close()
-        const exists = await fileExists('test/write.txt')
+        const exists = await fileExists('tests/deno/cli/write.txt')
         assertEquals(exists, false)
     }
 })
@@ -21,8 +21,8 @@ Deno.test({
     name: 'Should do nothing if no context titles are defined',
     async fn(): Promise<void> {
         const p = await Deno.run({
-            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../mod.ts', '../example/read.conf', 'write.txt'],
-            cwd: "./tests"
+            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../../../mod.ts', '../read.conf', 'write.txt'],
+            cwd: "./tests/deno/cli"
         })
         const status = await p.status()
         p.close()
@@ -35,14 +35,14 @@ Deno.test({
     name: 'Should do nothing if args are OK but no data was found in read file',
     async fn(): Promise<void> {
         const p = await Deno.run({
-            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../mod.ts', '../example/read.conf', 'write.txt', 'idontexist'],
-            cwd: "./tests"
+            cmd: ['deno', 'run', '--allow-read', '--allow-run', '../../../mod.ts', '../read.conf', 'write.txt', 'idontexist'],
+            cwd: "./tests/deno/cli"
         })
         const status = await p.status()
         p.close()
         assertEquals(status.code, 0)
         assertEquals(status.success, true)
-        assertEquals(await fileExists('tests/write.txt'), false)
+        assertEquals(await fileExists('tests/deno/cli/write.txt'), false)
     }
 })
 
@@ -50,17 +50,17 @@ Deno.test({
     name: 'Should extract the contexts when all arguments are correct',
     async fn(): Promise<void> {
         const p = await Deno.run({
-            cmd: ['deno', 'run', '--allow-read', '--allow-run', '--allow-write', '../mod.ts', '../example/read.conf', '../example/write.conf', 'version-1'],
-            cwd: "./tests"
+            cmd: ['deno', 'run', '--allow-read', '--allow-run', '--allow-write', '../../../mod.ts', '../read.conf', 'write.conf', 'version-1'],
+            cwd: "./tests/deno/cli"
         })
         const status = await p.status()
         p.close()
         assertEquals(status.code, 0)
         assertEquals(status.success, true)
-        assertEquals(await fileExists('example/write.conf'), true)
+        assertEquals(await fileExists('tests/deno/cli/write.conf'), true)
         const decoder = new TextDecoder()
-        const writeFileContent = decoder.decode(Deno.readFileSync(Deno.cwd() + "/example/write.conf"))
+        const writeFileContent = decoder.decode(Deno.readFileSync("tests/deno/cli/write.conf"))
         assertEquals(writeFileContent, "[version-1.1]\nname = Version 1.1\n[version-1.2]\nname = Version 1.2")
-        Deno.removeSync('example/write.conf')
+        Deno.removeSync('tests/deno/cli/write.conf')
     }
 })
